3d15fd978a2479880ab490cde8f4c811f71717fd
dombigreta <dombigreta96@gmail.com>
2022-06-12 16:42:29 +0200

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './ui-components/sidebar/sidebar.component';
+import {MaterialComponentsModule} from '../material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
